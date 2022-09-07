e257c354a2707e6c04a93524077c6aa6811b8227
jjjojnhyfc <113017443+jjjojnhyfc@users.noreply.github.com>
2022-09-07 20:04:09 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -6,23 +6,11 @@ export const config = {
   // 公众号APP_ID 
   // APP_ID: "wx39e4a624aea19caa",
   // 公众号APP_SECRET 
-  // APP_SECRET: "e2ba1c7fff43d25688923536ba6fee8",
-
+  // APP_SECRET: "977ec09fe07ca9228623d959d613b299",
+  //模版消息id
+  TEMPLATE_ID:"9sDI0RbZawu2Srgy2Ss4sYgJTMI8rq3O2I4_BiaAUG0 ",
   USERS: ["oQhUU5zWVw2j4IliXYv0ps_zeiTY"]
-    {
-      // 想要发送的人的名字
-      name: "妈咪",
-      // 扫码关注你的微信测试号后生成的一段字符串，在测试号后台能看到
-      id: "ohxOO6VskY2v40Kuf5ywIZVRM3puXhk",
-      // 你想对他发送的模板消息的模板ID
-      useTemplateId: "8mrvAMVgjbQ9LrPAThIs3XNOss_-7YwSrBlXly3vK2g",
-      // 所在省份
-      province: "广东",
-      // 所在城市
-      city: "广州",
-      // 他点击详情后跳转的页面,你可以设置成微博的热榜，也可以设置成其他，网址一定要填对；不填对也没关系，随便你，会打不开而已。
-      openUrl: "https://wangxinleo.cn"
-    
+   
   /**
    * 信息配置
    */
@@ -33,6 +21,3 @@ export const config = {
   PROVINCE: "广东",
   // 默认所在城市, USERS 中没填的话, 会默认拿这里的城市
   CITY: "广州",
-
-
-  
